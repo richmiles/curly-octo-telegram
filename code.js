52555e0b0b9678f2b0e3f297d0617705db81c9a9
Rich Miles <me@richmiles.xyz>
2023-12-08 14:54:08 -0700

    Changed var to let.

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 function insertionSortReverse(array) {
     // Start from the second-to-last element and move backwards
-for (var i = array.length - 2; i >= 0; i--) {
-  var val = array[i];
-  var j;
+for (let i = array.length - 2; i >= 0; i--) {
+  let val = array[i];
+  let j;
 
   // Move through the array to the right of the current position
   for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
