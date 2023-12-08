fd020883d37857749c1bf4ecb750f74a64ae3984
Rich Miles <me@richmiles.xyz>
2023-12-08 13:01:12 -0700

    Implemented a true reverse insertion sort.

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-function insertionSortReverse(arr) {    
-    return arr.sort(function(a, b)
-                { return a - b; }); 
+function insertionSortReverse(array) {
+    // Start from the second-to-last element and move backwards
+for (var i = arr.length - 2; i >= 0; i--) {
+  var val = arr[i];
+  var j;
+
+  // Move through the array to the right of the current position
+  for (j = i; j < arr.length - 1 && arr[j + 1] < val; j++) {
+    arr[j] = arr[j + 1];
+  }
+
+  arr[j] = val;
+}
+
+return arr;
 }
