248820d99d8cdd3ea2b29930ddc17b317c684206
Rich Miles <me@richmiles.xyz>
2023-12-08 13:04:49 -0700

    Fixed typo.

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ for (var i = array.length - 2; i >= 0; i--) {
 
   // Move through the array to the right of the current position
   for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
-    array[j] = arr[j + 1];
+    array[j] = array[j + 1];
   }
 
   array[j] = val;
