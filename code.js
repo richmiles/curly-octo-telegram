bbd7891c02ce56ee647ff938969fae341ae6528c
Rich Miles <me@richmiles.xyz>
2023-12-09 08:07:13 -0700

    Updated variable names for clarity.

--- code.js
+++ code.js
@@ -1,21 +1,22 @@
 // Function to perform insertion sort in reverse order.
 function insertionSortReverse(array) {
+
   // Start from the second-to-last element and move backwards
-  for (let i = array.length - 2; i >= 0; i--) {
+  for (let currentIndex = array.length - 2; currentIndex >= 0; currentIndex--) {
     // Store the value of the current element to be inserted in its correct position
-    const val = array[i];
+    const val = array[currentIndex];
 
     // Initialize j to the current index i
-    let j = i;
+    let shiftIndex = currentIndex;
     // Move through the array to the right of the current position
     // This loop shifts elements to the left until the correct position for val is found
-    for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
+    for (; shiftIndex < array.length - 1 && array[shiftIndex + 1] < val; shiftIndex++) {
       // Shift the element to the left
-      array[j] = array[j + 1];
+      array[shiftIndex] = array[shiftIndex + 1];
     }
 
     // Insert the value in its correct position
-    array[j] = val;
+    array[shiftIndex] = val;
   }
 
   // Return the sorted array
