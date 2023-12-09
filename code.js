8e691061c34eeb277d531f00c84b236ef3917889
Rich Miles <me@richmiles.xyz>
2023-12-09 07:53:19 -0700

    Added some minor comments.

--- code.js
+++ code.js
@@ -1,16 +1,19 @@
+// Code for the insertion sort algorithm,
+// but starting from the end of the array.
 function insertionSortReverse(array) {
-    // Start from the second-to-last element and move backwards
-for (let i = array.length - 2; i >= 0; i--) {
-  let val = array[i];
-  let j;
 
-  // Move through the array to the right of the current position
-  for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
-    array[j] = array[j + 1];
+  // Start from the second-to-last element and move backwards
+  for (let i = array.length - 2; i >= 0; i--) {
+    const val = array[i];
+
+    let j = i;
+    // Move through the array to the right of the current position
+    for (j = i; j < array.length - 1 && array[j + 1] < val; j++) {
+      array[j] = array[j + 1];      
+    }
+
+    array[j] = val;
   }
 
-  array[j] = val;
-}
-
-return array;
+  return array;
 }
